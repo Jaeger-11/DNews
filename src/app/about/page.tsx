b948ece89edc81013b35cb8b856b1bdde1e6c056
Oluwadamilola Falodun <dpaulzs11@gmail.com>
2024-09-18 17:58:47 +0100

    footer pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,66 @@
+import Layout from '@/components/Layout'
 import React from 'react'
+import Image from 'next/image'
 
 const page = () => {
   return (
-    <div>page</div>
+    <Layout>
+      <div className='text-dark p-2'>
+        <h3 className="font-primary text-primary font-semibold text-2xl mb-2">Who We Are</h3>
+        <p>Welcome to DNews, your go-to destination for staying informed with the latest news, trends, and stories from around the world. We&apos;re a team of passionate young professionals who are driven to make news accessible, reliable, and engaging.</p>
+
+        <section className="my-4 flex flex-col gap-4 [&_ul]:list-disc [&_li]:ml-4 [&_span]:font-semibold">
+          <section className='grid grid-cols-1 md:grid-cols-2 gap-4 items-center border-y py-4'>
+            <div>
+              <Image   
+              src={'/team.jpg'}
+              width={400}
+              height={200}
+              alt='team'
+              className='aspect-video w-full object-cover object-center'
+              />
+            </div>
+            <article>
+              <h2 className="text-xl capitalize font-semibold mb-1 text-primary font-primary">Our Story</h2>
+              <p>Founded by <i className='font-semibold text-primary'>Victor, Chisom, Dami, and Nafisa</i> <b className='text-primary'>(VCDN)</b>, DNews was created to bridge the gap between news content and the fast-paced lives of today&apos;s generation. We realized that while the news landscape is vast, it often lacks personalization and user interaction. So, we set out to build a platform that not only delivers breaking news but also fosters a community of readers who can engage, comment, and share their thoughts in real-time.</p>
+            </article>
+          </section>
+
+          <article>
+            <h2 className="text-xl capitalize font-semibold mb-2 text-primary font-primary">Our Mission</h2>
+            <p>At DNews, our mission is simple: <br />
+            <span>To keep you informed and connected to the world.</span> <br />
+            We believe that news should be accessible to everyone, presented in a modern, fast, and user-friendly way. Whether you want to catch up on global events, bookmark your favorite articles, or share your opinions with others, DNews is here to make that experience seamless.</p>
+          </article>
+
+          <article>
+            <h2 className="text-xl capitalize font-semibold mb-2 text-primary font-primary">What Sets Us Apart</h2>
+            <ul>
+              <li><span>Engaging and Interactive: </span>We don&apos;t just provide news; we encourage our users to participate in discussions, share their viewpoints, and engage with the content that matters most to them.</li>
+              <li><span>Personalized Experience: </span>From top trends to curated categories, DNews tailors news based on your preferences. Our platform gives you the power to explore news by your interests and bookmark articles for later.</li>
+              <li><span>Community-Driven:</span>We believe that news is more than just headlines. Our community shares opinions, debates key issues, and contributes to shaping the news narrative.</li>
+              <li><span>Fast and Lightweight:</span>Built with cutting-edge technology, DNews is designed to provide a fast, reliable, and mobile-friendly news experience.</li>
+            </ul>
+          </article>
+
+          <article>
+            <h2 className="text-xl capitalize font-semibold mb-2 text-primary font-primary">Meet the Team</h2>
+            <p>Behind DNews is a dynamic team of youth-driven innovators who bring passion and expertise to the table:</p>
+            <ul>
+              <li><span>Victor: </span>A tech enthusiast with a keen eye for innovation and problem-solving. He ensures DNews delivers the latest features that make our platform user-friendly.</li>
+              <li><span>Chisom:</span> A master of design and user experience. Chisom brings news to life visually and makes sure the platform looks as good as it works.</li>
+              <li><span>Dami: </span> A strategist who always has an ear to the ground on trending topics. Dami leads the content strategy to ensure you&apos;re always in the loop.</li>
+              <li><span>Nafisa:</span> Our community builder and outreach expert. Nafisa connects us to our audience, ensuring that DNews remains interactive and inclusive.</li>
+            </ul>
+          </article>
+            
+          <article>
+            <h2 className="text-xl capitalize font-semibold mb-2 text-primary font-primary">Join Us on Our Journey</h2>
+            <p>DNews is not just a news app; it&apos;s a growing community. Join us in our mission to stay informed, express opinions, and make a positive impact on the way news is consumed. Whether you&apos;re here for the latest breaking news, to engage in conversations, or simply to stay updated, DNews is the platform for you.</p>
+          </article>
+        </section>
+      </div>
+    </Layout>
   )
 }
 
--- src/app/advertise/page.tsx
+++ src/app/advertise/page.tsx
@@ -3,8 +3,67 @@ import Layout from "@/components/Layout"
 const page = () => {
   return (
     <Layout>
-      <div>
-        ADVERTISE
+      <div className='text-dark p-2'>
+        <h3 className="font-primary text-primary font-semibold text-2xl mb-2">Advertise With DNews</h3>
+        <p>Looking to reach a dynamic, engaged audience? DNews is the perfect platform for your brand to connect with a diverse and informed readership. With our fast-growing community and dedicated user base, advertising on DNews offers you a powerful way to promote your brand, product, or service to those who care about what&apos;s happening in the world.</p>
+
+        <section className="my-4 flex flex-col gap-4 [&_ul]:list-disc">
+          <article>
+            <h2 className="text-xl capitalize font-semibold mb-2 text-primary font-primary">Why Advertise with Us?</h2>
+            <div className="flex flex-col gap-4">
+              <div className="">
+                <span className="font-semibold">Highly Engaged Audience</span> <br />
+                <p>DNews attracts users who are actively engaging with news content, sharing opinions, and discussing current events. Our audience is diverse, well-informed, and highly responsive, making it an excellent target for your advertising campaigns.</p>
+              </div>
+              <div className="">
+                <span className="font-semibold">Tailored Advertising Solutions</span> <br />
+                <p>We understand that every brand is unique. That&apos;s why we offer tailored advertising packages that fit your specific needs and goals. Whether it&apos;s display ads, sponsored content, or native advertising, we work with you to create the most effective campaign to capture attention.</p>
+              </div>
+              <div>
+                <span className="font-semibold">Modern and Mobile-Friendly Platform</span> <br />
+                <p>With our lightweight, fast, and user-friendly interface, DNews ensures that your advertisements are displayed seamlessly across all devices, including mobile, tablet, and desktop. Reach users wherever they are, whenever they&apos;re tuned into the news.</p>
+              </div>
+              <div>
+                <span className="font-semibold">Trusted News Source</span><br />
+                <p>DNews is built on the principles of reliability, accuracy, and timely news delivery. Our users trust us to provide the information that matters, making our platform the perfect place to advertise products and services they&apos;ll appreciate.</p>
+              </div>
+            </div>
+          </article>
+
+          <article>
+            <h2 className="text-xl capitalize font-semibold mb-2 text-primary font-primary">Advertising Options</h2>
+            <p>We offer a variety of advertising formats to maximize your brand&apos;s visibility:</p>
+            <ul className="[&_li]:ml-4 list-disc">
+              <li><span className="font-semibold">Display Ads:</span>Engage our readers with high-impact banner ads placed strategically across our website and app.</li>
+              <li><span className="font-semibold">Sponsored Content:</span>Align your brand with relevant, engaging content that resonates with our users.</li>
+              <li><span className="font-semibold">Video Ads:</span>Capture attention with short, captivating video ads that play before news videos or within articles.</li>
+              <li><span className="font-semibold">Newsletter Sponsorship:</span>Get your brand directly into the inboxes of our most dedicated readers by sponsoring our newsletter.</li>
+              <li><span className="font-semibold">Custom Campaigns:</span>Work with our team to design a unique advertising campaign that fits your brand&apos;s needs and target audience.</li>
+            </ul>
+          </article>
+
+          <article>
+            <h2 className="text-xl capitalize font-semibold mb-2 text-primary font-primary">Analytics and Performance</h2>
+            <p>We provide detailed reporting and analytics to ensure you get the most out of your advertising campaigns. Track your ad&apos;s performance in real-time, analyze engagement metrics, and gain insights that help you refine your strategy for maximum impact.</p>
+          </article>
+
+          <article>
+            <h2 className="text-xl capitalize font-semibold mb-2 text-primary font-primary">Partner with DNews</h2>
+            <p>At DNews, we&apos;re not just about delivering news—we&apos;re about building partnerships that drive results. Whether you&apos;re a small business, a startup, or an established brand, we&apos;re here to help you connect with the right audience at the right time.</p>
+          </article>
+
+          <article>
+            <h2 className="text-xl capitalize font-semibold mb-2 text-primary font-primary">Get Started Today</h2>
+            <p>Ready to take your brand to the next level with DNews? Contact our advertising team to discuss your goals, and we&apos;ll create a custom solution that fits your needs.</p>
+          </article>
+
+          <ul className="[&_li]:ml-4 [&_span]:font-semibold">
+            <p>Contact Us</p>
+            <li><span>Email: </span><a href="mailto:ads@dnews.com" className="text-accent">ads@dnews.com</a> </li>        
+            <li><span>Phone: </span><a href="tel:+2348139017478" className="font-semibold">+ (234)8139017478</a></li>
+            <p>We look forward to helping your brand make an impact!</p>
+          </ul>
+        </section>
       </div>
     </Layout>
   )
